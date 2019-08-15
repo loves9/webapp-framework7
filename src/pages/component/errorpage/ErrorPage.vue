b048ec228bf5a2d3a8cb5b6eda388f18d7aaa0cb
loves9 <ifengfree@gmail.com>
2019-08-15 16:53:31 +0800

    [opt]

--- src/pages/component/errorpage/ErrorPage.vue
+++ src/pages/component/errorpage/ErrorPage.vue
@@ -1,118 +1,112 @@
 <template>
-    <div class="upgrade_container_cls" style="background-color:#fff; height:100vh">
-        <img class="upgrade_img_cls" :src="imagePath" />
+  <div class="upgrade_container_cls" style="background-color:#fff; height:100vh">
+    <img class="upgrade_img_cls" :src="imagePath" />
 
-        <div class="upgrade_text_container_cls">
-            <span class="label_one ">{{labelText_one}}</span>
-            <span class="label_two">尊敬的用户，您好！</span>
-            <span class="label_two">{{labelText_two}}</span>
-            <span class="label_two">{{labelText_three}}</span>
-            <span class="label_three">服务电话：010-59618206</span>
-        </div>
-
-        <div class="error_btn_cls">
-            <x-button style="background-color:#fff" @click.native="errorButtonClick">{{buttonTitle}}</x-button>
-        </div>
+    <div class="upgrade_text_container_cls">
+      <span class="label_one">{{labelText_one}}</span>
+      <span class="label_two">尊敬的用户，您好！</span>
+      <span class="label_two">{{labelText_two}}</span>
+      <span class="label_two">{{labelText_three}}</span>
+      <span class="label_three">服务电话：010-59618206</span>
     </div>
 
+    <div class="error_btn_cls">
+      <x-button style="background-color:#fff" @click.native="errorButtonClick">{{buttonTitle}}</x-button>
+    </div>
+  </div>
 </template>
 
 <script>
-import {
-  XButton
-} from 'vux'
+import { XButton } from "vux";
 export default {
   props: {
     type: {
       type: String,
-      default: 'system'
+      default: "system"
     },
     buttonTitle: {
       type: String,
-      default: '确定'
+      default: "确定"
     }
   },
-  data () {
+  data() {
     return {
-      labelText_one: '哎呀，出错了…',
-      labelText_two: '由于系统原因给您造成不便，',
-      labelText_three: '请联系技术支持协助解决。',
-      imagePath: require('./images/error.png')
-    }
+      labelText_one: "哎呀，出错了…",
+      labelText_two: "由于系统原因给您造成不便，",
+      labelText_three: "请联系技术支持协助解决。",
+      imagePath: require("./images/error.png")
+    };
   },
-  mounted () {
+  mounted() {
     switch (this.type) {
-      case 'system':
-
-        break
-      case 'upgrade':
-        this.labelText_one = '精彩内容，稍后呈现…'
-        this.labelText_two = '由于目前系统升级中，'
-        this.labelText_three = '给您造成不便，敬请谅解。'
-        this.imagePath = require('./images/upgrade.png')
-        break
+      case "system":
+        break;
+      case "upgrade":
+        this.labelText_one = "精彩内容，稍后呈现…";
+        this.labelText_two = "由于目前系统升级中，";
+        this.labelText_three = "给您造成不便，敬请谅解。";
+        this.imagePath = require("./images/upgrade.png");
+        break;
     }
   },
-  computed: {
-
-  },
+  computed: {},
   methods: {
-    errorButtonClick () {
-      this.$emit('HREVENT_errorButtonClick')
+    errorButtonClick() {
+      this.$emit("HREVENT_errorButtonClick");
     }
   },
   components: {
     XButton
   }
-}
+};
 </script>
 
 <style lang="less">
 .upgrade_container_cls {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 }
 
 .upgrade_text_container_cls {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    width: 100%
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
 }
 
 .upgrade_img_cls {
-    width: 239px;
-    // height: 196px;
-    margin-top: 50px;
+  width: 239px;
+  // height: 196px;
+  margin-top: 50px;
 }
 .label_one {
-    color: #346dc7;
-    font-size: 24px;
-    margin-top: 40px;
-    padding-left: 89px;
-    margin-bottom: 20px;
-    font-family: "PingFangSC-Medium";
+  color: #346dc7;
+  font-size: 24px;
+  margin-top: 40px;
+  padding-left: 89px;
+  margin-bottom: 20px;
+  font-family: "PingFangSC-Medium";
 }
 .label_two {
-    color: #303133;
-    font-size: 16px;
-    margin-top: 10px;
-    padding-left: 89px;
-    font-family: "PingFangSC-Regular";
+  color: #303133;
+  font-size: 16px;
+  margin-top: 10px;
+  padding-left: 89px;
+  font-family: "PingFangSC-Regular";
 }
 
 .label_three {
-    color: #346dc7;
-    font-size: 16px;
-    margin-top: 10px;
-    padding-left: 89px;
-    font-family: "PingFangSC-Medium";
+  color: #346dc7;
+  font-size: 16px;
+  margin-top: 10px;
+  padding-left: 89px;
+  font-family: "PingFangSC-Medium";
 }
 .error_btn_cls {
-    width: 90%;
-    // padding: 15px;
-    margin-top: 90vh;
-    position: absolute;
+  width: 90%;
+  // padding: 15px;
+  margin-top: 90vh;
+  position: absolute;
 }
 </style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -4,18 +4,11 @@
             <f7-nav-left back-link color="white"></f7-nav-left>
             <f7-nav-title>About</f7-nav-title>
     </f7-navbar>-->
-    <f7-block-title>My App</f7-block-title>
-    <f7-block strong>
-      <p>Here is Home page!</p>
-      <p>
-        You can go
-        <f7-link back>back</f7-link>.
-      </p>
-      <p>Mauris posuere sit amet metus id venenatis. Ut ante dolor, tempor nec commodo rutrum, varius at sem. Nullam ac nisi non neque ornare pretium. Nulla mauris mauris, consequat et elementum sit amet, egestas sed orci. In hac habitasse platea dictumst.</p>
-      <p>Fusce eros lectus, accumsan eget mi vel, iaculis tincidunt felis. Nulla tincidunt pharetra sagittis. Fusce in felis eros. Nulla sit amet aliquam lorem, et gravida ipsum. Mauris consectetur nisl non sollicitudin tristique. Praesent vitae metus ac quam rhoncus mattis vel et nisi. Aenean aliquet, felis quis dignissim iaculis, lectus quam tincidunt ligula, et venenatis turpis risus sed lorem. Morbi eu metus elit. Ut vel diam dolor.</p>
-    </f7-block>
+    <f7-block-title>HOME</f7-block-title>
 
-    <f7-button fill raised @click="next">确定</f7-button>
+
+
+    <f7-button fill raised @click="next">下一页</f7-button>
   </f7-page>
 </template>
 
@@ -29,7 +22,7 @@ export default {
   },
   methods: {
     next() {
-      this.easyPush("/form/", {
+      this.easyPush("/about/", {
         name: "About",
         index: 2
       });
